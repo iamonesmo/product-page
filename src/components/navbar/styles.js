884fe9e8d4333a0e6c8d4dbbe360d3283f1a5e4f
Onesmo Kasongo <iamonesmo@gmail.com>
2022-01-01 11:32:01 +0530

    initial commit

---
+++ src/components/navbar/styles.js
@@ -0,0 +1,15 @@
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    backgroundColor: "white",
+    height: "120px",
+    // color: "#1976d2",
+    borderBottom: "solid #eeeeee 1px",
+  },
+  logo: {
+    maxWidth: 160,
+  },
+}));
+
+export default useStyles;
